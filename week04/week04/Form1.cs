bb15f269608db1d835a2702a6ca8be17789f7e02
Zoltán Tomcsik <tomzol313@gmail.com>
2020-10-02 16:40:40 +0200

    8

--- week04/week04/Form1.cs
+++ week04/week04/Form1.cs
@@ -10,6 +10,8 @@ using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
 using System.IO;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+using Microsoft.SqlServer.Server;
 
 namespace week04
 {
@@ -107,6 +109,28 @@ namespace week04
                 xlSheet.get_Range(
                 GetCell(2, 1),
                 GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+                int lastRowID = xlSheet.UsedRange.Rows.Count;
+
+                Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+                headerRange.Font.Bold = true;
+                headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+                headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                headerRange.EntireColumn.AutoFit();
+                headerRange.RowHeight = 40;
+                headerRange.Interior.Color = Color.LightBlue;
+                headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+                Excel.Range tableRange = xlSheet.get_Range(GetCell(1, 1), GetCell(lastRowID, headers.Length));
+                tableRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+                Excel.Range firstColumnRange = xlSheet.get_Range(GetCell(1, 1), GetCell(lastRowID, 1));
+                firstColumnRange.Font.Bold = true;
+                firstColumnRange.Interior.Color = Color.LightYellow;
+
+                Excel.Range lastColumnRange = xlSheet.get_Range(GetCell(1, headers.Length), GetCell(lastRowID, headers.Length));
+                lastColumnRange.Interior.Color = Color.LightGreen;
+                lastColumnRange.NumberFormat = Math.Round(lastColumnRange.Value, 2);
             }
         }
 
