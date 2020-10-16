b1ae24d82f12ecd25614a1551e35138df583815d
Zoltán Tomcsik <tomzol313@gmail.com>
2020-10-16 16:18:46 +0200

    5

--- Webszolgaltatas/Webszolgaltatas/Form1.cs
+++ Webszolgaltatas/Webszolgaltatas/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using Webszolgaltatas.Entities;
 using Webszolgaltatas.MnbServiceReference;
 
@@ -16,10 +17,13 @@ namespace Webszolgaltatas
     {
         BindingList<RateData> Rates = new BindingList<RateData>();
 
+        var response = mnbService.GetExchangeRates(request);
+        var result = response.GetExchangeRatesResult;
         public Form1()
         {
             InitializeComponent();
             GetExchangeRates();
+            GetXML();
 
             dataGridView1.DataSource = Rates;
         }
@@ -34,9 +38,25 @@ namespace Webszolgaltatas
                 startDate = "2020-01-01",
                 endDate = "2020-06-30"
             };
+        }
 
-            var response = mnbService.GetExchangeRates(request);
-            var result = response.GetExchangeRatesResult;
+        private void GetXML()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
     }
 }
