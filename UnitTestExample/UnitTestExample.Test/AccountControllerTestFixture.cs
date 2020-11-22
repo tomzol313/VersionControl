30eb12521eb1840be2050aec68961e8f163aacc6
Zoltán Tomcsik <tomzol313@gmail.com>
2020-11-22 20:51:17 +0100

    4

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace UnitTestExample.Test
 {
     class AccountControllerTestFixture
     {
+        [Test]
+        private void TestValidateEmail(string email, bool expectedResult)
+        {
+
+        }
     }
 }
