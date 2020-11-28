a2daea112c73626a306b2204a9cefc5a0155c6b3
Zoltán Tomcsik <tomzol313@gmail.com>
2020-11-28 20:51:44 +0100

    6

--- EvoluciosAlgoritmus/EvoluciosAlgoritmus/Form1.cs
+++ EvoluciosAlgoritmus/EvoluciosAlgoritmus/Form1.cs
@@ -35,11 +35,6 @@ namespace EvoluciosAlgoritmus
                 gc.AddPlayer(nbrOfSteps);
             }
             gc.Start();
-
-            var playerList = from p in gc.GetCurrentPlayers()
-                             orderby p.GetFitness() descending
-                             select p;
-            var topPerformers = playerList.Take(populationSize / 2).ToList();
         }
 
         private void Gc_GameOver(object sender)
@@ -48,6 +43,27 @@ namespace EvoluciosAlgoritmus
             label1.Text = string.Format(
                 "{0}. generáció",
                 generation);
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
         }
     }
 }
