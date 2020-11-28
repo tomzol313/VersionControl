6c70975a92636ddd69f4ea05ca2a1b28c5f87f03
Zoltán Tomcsik <tomzol313@gmail.com>
2020-11-28 19:48:09 +0100

    3

--- EvoluciosAlgoritmus/EvoluciosAlgoritmus/Form1.cs
+++ EvoluciosAlgoritmus/EvoluciosAlgoritmus/Form1.cs
@@ -22,6 +22,8 @@ namespace EvoluciosAlgoritmus
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+            gc.AddPlayer();
+            gc.Start(true);
         }
     }
 }
